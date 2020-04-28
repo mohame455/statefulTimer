960cb3dcb5cc95a33db3dc0e6190cd54da67299c
mohame455 <61824199+mohame455@users.noreply.github.com>
2020-04-28 14:41:43 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,63 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sec: 0,
+      isOn: false
+    }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+  startTime = () => {
+    this.second=setInterval(
+      () => {
+        this.setState({
+          sec: this.state.sec + 1
+        })
+      },
+      1000
+    )
+    this.setState({
+      isOn:true
+    })
+  }
+  stopTime=()=>{
+    clearInterval(this.second)
+    this.setState({
+      isOn:false
+    })
+  }
+
+  resetTime=()=>{
+    clearInterval(this.second)
+    this.setState({
+      sec:0
+    })
+  }
+
+  displayTime(timeSec){
+    let num=timeSec/3600
+    let h=Math.floor(num)
+    let m=Math.floor((num-h)*60)
+    let s=timeSec-((h*3600)+m*60)
+    return<div>
+      <span>{String(h).padStart(2,0)} : {String(m).padStart(2,0)} : {String(s).padStart(2,0)}</span>
     </div>
-  );
+  }
+
+
+  render() {
+    return (<div className="main">
+      <p className="time">{this.displayTime(this.state.sec)}</p>
+      <div className='time-button'>
+        <button onClick={!this.state.isOn ? this.startTime : this.stopTime}>Start</button>
+        <button onClick={this.resetTime}>Reset</button>
+      </div>
+    </div>);
+  }
 }
 
 export default App;
